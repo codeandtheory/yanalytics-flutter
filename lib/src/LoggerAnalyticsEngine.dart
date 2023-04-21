45952b6fb7d7b8f6030c1e7e770ce536b66b3de5
EduNavaYML <122565743+EduNavaYML@users.noreply.github.com>
2023-04-21 15:44:30 -0600

    [CM-1370] Add LoggerAnalyticsEngine

---
+++ lib/src/LoggerAnalyticsEngine.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+
+import 'analytics_engine.dart';
+
+class LoggerAnalyticsEngine extends AnalyticsEngine
+{
+  Logger get logger => Logger("co.yml.yanalytics.Core", "Analytics");
+
+  @override
+  void trackEvent(AnalyticsEvent event) {
+    if (event.screenName != null)
+    {
+      String? screenName = event.screenName;
+      logger.log("Screen view: $screenName");
+    }
+    else if (event.userPropertyName != null)
+    {
+      String? userPropertyName = event.userPropertyName;
+      String? userPropertyValue = event.userPropertyValue;
+      logger.log("User property:: $userPropertyName = $userPropertyValue");
+    }
+    else if (event.eventName != null)
+    {
+      String? eventName = event.eventName;
+      Map<String, dynamic>? eventParameters = event.eventParemeters;
+      if (event.eventParemeters != null)
+      {
+        logger.log("Event: $eventName, $eventParameters");
+      }
+      else
+      {
+        logger.log("Event: $eventName");
+      }
+    }
+  }
+
+  @override
+  void trackEventFactory(AnalyticsEventFactory factory) {
+    if (factory.event.eventName != null)
+    {
+      String? screenName = factory.event.screenName;
+      logger.log("Screen view: $screenName");
+    }
+    else if (factory.event.userPropertyName != null)
+    {
+      String? userPropertyName = factory.event.userPropertyName;
+      String? userPropertyValue = factory.event.userPropertyValue;
+      logger.log("User property:: $userPropertyName = $userPropertyValue");
+    }
+    else if (factory.event.eventName != null)
+    {
+      String? eventName = factory.event.eventName;
+      Map<String, dynamic>? eventParameters = factory.event.eventParemeters;
+      if (factory.event.eventParemeters != null)
+      {
+        logger.log("Event: $eventName, $eventParameters");
+      }
+      else
+      {
+        logger.log("Event: $eventName");
+      }
+    }
+  }
+}
+
+class Logger {
+  String _subsystem;
+  String _category;
+
+  Logger(this._subsystem, this._category);
+
+  void log(String message) {
+    print('[$_subsystem/$_category]: $message');
+  }
+}
