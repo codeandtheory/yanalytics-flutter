0b76683f8dcbd689ff29f9ce0265ee12ee3050a1
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-24 14:57:11 -0500

    [CM-1382] Fix documentation generation github action

--- lib/src/analytics_engine.dart
+++ lib/src/analytics_engine.dart
@@ -1,3 +1,4 @@
+/// Represents any event we might wish to track via analytics
 class AnalyticsEvent {
   final String? userPropertyValue;
   final String? userPropertyName;
@@ -5,18 +6,29 @@ class AnalyticsEvent {
   final String? eventName;
   final Map<String, dynamic>? eventParemeters;
 
+  /// Screen view event
+  ///
+  /// [screenName] screen name
   AnalyticsEvent.screenView({required this.screenName})
       : userPropertyName = null,
         userPropertyValue = null,
         eventName = null,
         eventParemeters = null;
 
+  /// User property event
+  ///
+  /// [name] user property name
+  /// [value] user property value
   AnalyticsEvent.userProperty(
       {required this.userPropertyName, required this.userPropertyValue})
       : screenName = null,
         eventName = null,
         eventParemeters = null;
 
+  /// Generic event
+  ///
+  /// [name] event name
+  /// [parameters] event metadata to track
   AnalyticsEvent.event({required this.eventName, this.eventParemeters})
       : screenName = null,
         userPropertyName = null,
@@ -27,8 +39,16 @@ abstract class AnalyticsEventFactory {
   AnalyticsEvent get event;
 }
 
+/// Generic analytics functions to abstract away from specific implementations (Firebase etc)
 abstract class AnalyticsEngine {
+  /// Track an analytics event
+  ///
+  /// [event] the event to log
   void trackEvent(AnalyticsEvent event);
+
+  /// Tracks an analytics event
+  ///
+  /// [factory] object that generates the event to log
   void trackEventFactory(AnalyticsEventFactory factory) {
     trackEvent(factory.event);
   }
