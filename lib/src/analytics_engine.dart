a5bee2b41c657ffc9a9441f81e4837b6d6260863
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2023-04-24 10:34:05 -0500

    [CM-1370] Implement basic analytics engines (#4)

---
+++ lib/src/analytics_engine.dart
@@ -0,0 +1,35 @@
+class AnalyticsEvent {
+  final String? userPropertyValue;
+  final String? userPropertyName;
+  final String? screenName;
+  final String? eventName;
+  final Map<String, dynamic>? eventParemeters;
+
+  AnalyticsEvent.screenView({required this.screenName})
+      : userPropertyName = null,
+        userPropertyValue = null,
+        eventName = null,
+        eventParemeters = null;
+
+  AnalyticsEvent.userProperty(
+      {required this.userPropertyName, required this.userPropertyValue})
+      : screenName = null,
+        eventName = null,
+        eventParemeters = null;
+
+  AnalyticsEvent.event({required this.eventName, this.eventParemeters})
+      : screenName = null,
+        userPropertyName = null,
+        userPropertyValue = null;
+}
+
+abstract class AnalyticsEventFactory {
+  AnalyticsEvent get event;
+}
+
+abstract class AnalyticsEngine {
+  void trackEvent(AnalyticsEvent event);
+  void trackEventFactory(AnalyticsEventFactory factory) {
+    trackEvent(factory.event);
+  }
+}
